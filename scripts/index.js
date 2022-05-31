bedd503b7167ae9c4e6c73b834eb1fcbe3f23809
StanislavCh <ahtuxakep@yandex.ru>
2022-05-31 17:19:12 +0300

    add comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,35 @@
-const popup = document.querySelector(".popup");
-const buttonEdit = document.querySelector(".profile__edit");
-const formElement = document.querySelector('.popup__form')
-const buttonClose = document.querySelector('.popup__close')
-let profileName = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__job');
-let profileNameInpute = formElement.querySelector('.popup__inpute_name');
-let jobInpute = formElement.querySelector('.popup__inpute_job');
+const popup = document.querySelector(".popup"); //Ищем попап
+const buttonEdit = document.querySelector(".profile__edit");  //Ищем кнопку изменить
+const formElement = document.querySelector('.popup__form');  //Ищем форму в попапе
+const buttonClose = document.querySelector('.popup__close');  //Ищем кнопку закрытия попапа
+let profileName = document.querySelector('.profile__name');  //Ищем имя пользователя
+let job = document.querySelector('.profile__job');  //Ищем работу пользователя
+let profileNameInpute = formElement.querySelector('.popup__inpute_name');  //Получаем введённое имя пользователя
+let jobInpute = formElement.querySelector('.popup__inpute_job');  //Получаем введённую работу пользователя
 
+//функция открытия попапа
 function showInfo() {
-  popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');  //Добавляем попапу класс для видимости
 
-  profileNameInpute.value = profileName.textContent;
-  jobInpute.value = job.textContent;
+  profileNameInpute.value = profileName.textContent;  //Получаем со страницы имя пользователя
+  jobInpute.value = job.textContent;  //Получаем со страницы работу пользователя
 }
 
+//фунция закрытия попапа
 function closeInfo() {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened'); //Удаляем класс видимости попапа
 }
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
+//функция для откправки данных формы
+function submitFormHandler (evt) { //Учту, но прежнее название было взято из задания на работу, там написано именно так, как было
+  evt.preventDefault();  // Эта строчка отменяет стандартную отправку формы.
 
-  profileName.textContent = profileNameInpute.value;
-  job.textContent = jobInpute.value;
+  profileName.textContent = profileNameInpute.value;  //Получаем введённое имя пользователя
+  job.textContent = jobInpute.value;  //Получаем введённую работу пользователя
 
-  closeInfo()
+  closeInfo()  //После отправки данных закрываем форму
 }
 
-buttonEdit.addEventListener("click", showInfo);
-buttonClose.addEventListener("click", closeInfo)
-formElement.addEventListener('submit', formSubmitHandler);
+buttonEdit.addEventListener("click", showInfo);  //Слушатель нажатия кнопки изменить
+buttonClose.addEventListener("click", closeInfo)  //Слушатель нажания кнопки закрыть
+formElement.addEventListener('submit', submitFormHandler);  //Слушатель нажатия кнопки сохранить
